071ddb9dac17720c5054dd2b027340dc92c333b2
baasuthakuri <97420645+baasuthakuri@users.noreply.github.com>
2022-07-31 20:59:24 +0545

    Add files via upload

---
+++ Login-UI-flutter/build/app/generated/source/buildConfig/debug/com/snapsid/loginuicolors/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.snapsid.loginuicolors;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.snapsid.loginuicolors";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
